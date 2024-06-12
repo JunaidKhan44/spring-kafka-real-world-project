4391f2045d26c2291f7be08d289fa057b4dd21cb
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-06-12 16:18:30 +0500

    Update WikiMediaRepository.java

--- spring-kafka-real-world-project/kafka-comsumer-database/src/main/java/com/jk/springboot/repository/WikiMediaRepository.java
+++ spring-kafka-real-world-project/kafka-comsumer-database/src/main/java/com/jk/springboot/repository/WikiMediaRepository.java
@@ -3,6 +3,11 @@ package com.jk.springboot.repository;
 import com.jk.springboot.entity.WikiMediaModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * @author Junaid.Khan
+ *
+ */
+
 public interface WikiMediaRepository
         extends JpaRepository<WikiMediaModel,Long> {
 }
