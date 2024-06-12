88c908b786e35d99a558207c229d3d45dfa527cd
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-06-12 16:19:43 +0500

    Update SpringBootProducerApplication.java

--- spring-kafka-real-world-project/kafka-producer-wikimedia/src/main/java/com/jk/springboot/SpringBootProducerApplication.java
+++ spring-kafka-real-world-project/kafka-producer-wikimedia/src/main/java/com/jk/springboot/SpringBootProducerApplication.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 
+/**
+ * @author Junaid.Khan
+ *
+ */
+
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class SpringBootProducerApplication implements CommandLineRunner {
 
